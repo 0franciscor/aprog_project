0e6a22d4f5dc30765fb367461a4740c7ab8514e4
Eduardo Gonçalves <1201238@isep.ipp.pt>
2020-12-04 15:56:35 +0000

    alinea a

--- src/Trabalho.java
+++ src/Trabalho.java
@@ -1,7 +1,25 @@
+import java.util.Scanner;
 public class Trabalho {
+    static Scanner ler = new Scanner(System.in);
+
     public static void main(String[] args) {
 
-        System.out.println("o chico gosta de pila");
-        System.out.println();
+        int n; //n = ordem da matriz (ex 4x4)
+        do{
+            n=ler.nextInt();
+        }while(n>=0);
+
+        int [][] matriz = new int [n][n];
+
+    }
+
+    public static void alineaA (int [][] matriz){
+
+        /*for {
+
+        }
+
+
+         */
     }
 }
