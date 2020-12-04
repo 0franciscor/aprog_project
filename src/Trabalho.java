23aaf5e62921d6e0305046945abba4790c23cfc0
FranciscoRedol <1201239@isep.ipp.pt>
2020-12-04 15:55:48 +0000

    Alinea B resolvida

--- src/Trabalho.java
+++ src/Trabalho.java
@@ -1,7 +1,16 @@
+import java.util.Scanner;
+
 public class Trabalho {
+
     public static void main(String[] args) {
 
-        System.out.println("o chico gosta de pila");
-        System.out.println();
     }
+    /*==========================================================*/
+    public static void alineaB(){
+        for(int i=0; i < n ; i++)
+            for(int j=0; j < n; j++)
+                System.out.println(matriz[i][j]);
+    }
+
+
 }
