6c457243a574f631da6137bac5fb95c7973e4228
Eduardo Gonçalves <1201238@isep.ipp.pt>
2020-12-04 18:12:44 +0000

    no message

--- src/Trabalho.java
+++ src/Trabalho.java
@@ -4,6 +4,8 @@ public class Trabalho {
 
     public static void main(String[] args) {
 
+        System.out.println("Quadro com numeros");
+
         int n; //n = ordem da matriz (ex 4x4)
         do{
             n=ler.nextInt();
@@ -12,31 +14,32 @@ public class Trabalho {
         int [][] matriz = new int [n][n];
         alineaA(matriz,n);
         alineaB(matriz,n);
-
     }
-
-
-
 
     public static void alineaA (int [][] matriz,int n){
 
-        for(int i=0; i < n ; i++)
-            for(int j=0; j < n; j++){
-                matriz [i][j]= ler.nextInt();
-            }
-
-
-
+        for(int i=0; i < n ; i++){
+            int num = ler.nextInt();
+                // matriz[i][j]= Integer.valueOf(String.valueOf(String.valueOf(num).charAt(j))); precisa do ciclo for em função de j
+                int aux1 = num;
+                int aux2 = n;
+                do {
+                    int dig = aux1 % 10;
+                    int aux = dig;
+                    aux2--;
+                    matriz[i][aux2]=aux;
+                    aux1 = aux1 / 10;
+                }while (aux1>0);
+        }
     }
     /*==========================================================*/
     public static void alineaB(int [][] matriz, int n){
 
         for(int i=0; i < n ; i++){
-            System.out.println();
+            if (i!=0)
+                System.out.println();
             for(int j=0; j < n; j++)
                 System.out.print(matriz[i][j]);
         }
-
     }
-
 }
