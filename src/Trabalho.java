7b3ca4a7cb47b5726ce20df3c54b8a35e1a58bb4
FranciscoRedol <1201239@isep.ipp.pt>
2020-12-11 00:07:33 +0000

    Alinea C Concluida e D por concluir. Upgrade de eficiencia na A

--- src/Trabalho.java
+++ src/Trabalho.java
@@ -4,40 +4,39 @@ import java.util.Scanner;
 public class Trabalho {
 
     public static void main(String[] args) throws FileNotFoundException {
-        alineaB(alineaA());
+        int[][] matriz = alineaA();
+        alineaB(matriz);
+        int [][] matrizFiltro = alineaC(matriz);
+        System.out.println("\nd)\n" + alineaD(matriz, matrizFiltro));
     }
 
     public static int[][] alineaA () throws FileNotFoundException {
         File inputMatriz = new File("Imagem.txt");
         Scanner ler = new Scanner(inputMatriz);
-        int numLinhas = 0;
+
+        int linhaCorrente = 0;
+        int colunaCorrente = 0;
+        int[][] matriz = null;
+
         while (ler.hasNext()) {
-            String readLinhas = ler.nextLine();
-            numLinhas++;
-        }
-        ler.close();
-
-        int[][] matriz = new int[(numLinhas - 2)][(numLinhas - 2)];
-
-        Scanner read = new Scanner(inputMatriz);
-        int aux = 0;
-        int aux2 = 0;
-        while (read.hasNext()) {
-            aux++;
-            String input = read.nextLine();
+            linhaCorrente++;
+            String input = ler.nextLine();
             System.out.println(input);
-            if (aux > 2) {
-                for (int j = 0; j < matriz.length; j++)
-                    matriz[aux2][j] = Integer.valueOf(String.valueOf(String.valueOf(input).charAt(j)));
-                aux2++;
+            if(linhaCorrente == 2){
+                int numLinhas = Integer.valueOf(input);
+                matriz = new int[numLinhas][numLinhas];
+            }
+            if (linhaCorrente > 2) {
+                for (int i = 0; i < matriz.length; i++)
+                    matriz[colunaCorrente][i] = Integer.valueOf(String.valueOf(String.valueOf(input).charAt(i)));
+                colunaCorrente++;
             }
         }
-        read.close();
         return matriz;
     }
 
-    public static void alineaB(int [][] matriz){
-        System.out.println("\nb)");
+    public static void alineaB(int[][] matriz){
+        System.out.println("b)");
         for(int i=0; i < matriz.length ; i++){
             if (i!=0)
                 System.out.println();
@@ -45,4 +44,40 @@ public class Trabalho {
                 System.out.print(matriz[i][j]);
         }
     }
+
+    public static int[][] alineaC(int[][] matriz) {
+        int[][] matrizFiltro = matriz;
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz.length; j++) {
+                matrizFiltro[i][j] = matriz[i][j];
+            }
+        }
+        if (matriz.length > 2) {
+            for (int i = 1; i < (matrizFiltro.length - 1); i++) {
+                for (int j = 1; j < (matrizFiltro.length - 1); j++) {
+                    matrizFiltro[i][j] = (matriz[i][j] + matriz[i][j - 1] + matriz[i][j + 1] + matriz[i - 1][j] + matriz[i + 1][j]) / 5; //requer mais teste
+                }
+            }
+            System.out.println("\nc)");
+            for (int i = 0; i < matriz.length; i++) {
+                if (i != 0)
+                    System.out.println();
+                for (int j = 0; j < matriz.length; j++)
+                    System.out.print(matriz[i][j]);
+            }
+        }
+            return matrizFiltro;
+    }
+
+    public static boolean alineaD(int[][] matriz, int[][] matrizFiltro){
+        boolean flag = true;
+        for(int i = 0; i<matriz.length; i++){
+            for(int j = 0; j< matriz.length; j++){
+                if (matriz[i][j] != matrizFiltro[i][j])
+                    flag = false;
+            }
+        }
+        return flag;
+    }
 }
+
